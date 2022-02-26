d556276ff7575f00d8cf4064ad2a2eee9e187c5e
Zi-Zhuo <zizhuolee@gmail.com>
2022-02-26 16:59:15 +0800

    Added code to q3

--- Question3.py
+++ Question3.py
@@ -1,4 +1,14 @@
 strUnit = input ("Please enter D for distance, A for area or V for volume: ")
 
-print(strUnit)
+#print(strUnit)
+
+if strUnit == 'D':
+    print("Distance")
+elif strUnit == "A":
+    print ("Area")
+elif strUnit == "V":
+    print("Volume")
+else:
+    print("Invalid Choice")
+    quit()
 
