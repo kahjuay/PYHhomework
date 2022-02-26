d00dad3e2ca723056dc1a50705b7c469747707df
Lee Kah Juay <kahjuay@gmail.com>
2022-02-26 17:12:32 +0800

    Add IntQty

--- Question3.py
+++ Question3.py
@@ -1,6 +1,14 @@
+intQty = input ("Please enter an integer for Qty: ")
 strUnit = input ("Please enter D for distance, A for area or V for volume: ")
 
 #print(strUnit)
+
+try:
+    intQty = int(intQty)
+    print (str(intQty) + " is an Integer")
+except:
+    print (intQty + " is not an Integer")
+    quit()
 
 if strUnit == 'D':
     print("Distance")
