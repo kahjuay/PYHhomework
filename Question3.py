e3c1185d0400e8f224362c76068c29e2d214e21f
Zi-Zhuo <zizhuolee@gmail.com>
2022-02-26 17:11:08 +0800

    added initial unit of measurement

--- Question3.py
+++ Question3.py
@@ -7,8 +7,18 @@ if strUnit == 'D':
 elif strUnit == "A":
     print ("Area")
 elif strUnit == "V":
-    print("Volume2")
+    print("Volume")
 else:
     print("Invalid Choice")
     quit()
+
+unit_of_measurement = input("Input Unit of Measurement- i.e. mm, cm, m ,km")
+    if unit_of_measurement == "mm":
+        print("millimeter")
+    elif unit_of_measurement == "cm"
+        print("centimeter")
+    elif unit_of_measurement == "m"
+        print("meter")
+    else:
+        print("Invalid Unit")
 
