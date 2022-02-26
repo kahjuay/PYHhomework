5c6e8373b2d208ba04996092a6d45809d5739bba
Zi-Zhuo <zizhuolee@gmail.com>
2022-02-26 17:14:26 +0800

    added initial unit of measurement

--- Question3.py
+++ Question3.py
@@ -13,12 +13,12 @@ else:
     quit()
 
 unit_of_measurement = input("Input Unit of Measurement- i.e. mm, cm, m ,km")
-    if unit_of_measurement == "mm":
-        print("millimeter")
-    elif unit_of_measurement == "cm"
-        print("centimeter")
-    elif unit_of_measurement == "m"
-        print("meter")
-    else:
-        print("Invalid Unit")
+if unit_of_measurement == "mm":
+    print("millimeter")
+elif unit_of_measurement == "cm":
+    print("centimeter")
+elif unit_of_measurement == "m":
+    print("meter")
+else:
+    print("Invalid Unit")
 
