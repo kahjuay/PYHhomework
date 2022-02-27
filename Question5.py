f8b1e65885357118ff8bedb93c69c21846ce8ee3
Zi-Zhuo <zizhuolee@gmail.com>
2022-02-27 13:47:01 +0800

    added mark_1 & mark_2

--- Question5.py
+++ Question5.py
@@ -2,3 +2,6 @@ mark_1 = [46 , 41, 74, 53, 31, 39, 49, 57, 76, 80, 78, 38, 31, 56, 98, 55, 41, 7
 print(len(mark_1),"Digits in list")
 mark_2 = [69, 48, 50, 95, 87, 11, 28, 36, 25, 70, 25, 54, 33, 42, 27, 24, 44, 20, 61, 43, 30, 44, 7, 21]
 print(len(mark_2),"Digits in list")
+
+mark_1.sort(reverse=True)
+mark_2.sort(reverse=True)
